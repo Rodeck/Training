fbed74a40cf4c4d72dfc7b9e24c127715a3f490f
BadylTheWood <badylthewood@gmail.com>
2017-11-20 20:01:15 +0100

    dodalem przycisk

--- Training/Training/JakasNazwa.Designer.cs
+++ Training/Training/JakasNazwa.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // checkBox1
@@ -51,11 +52,21 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Elo";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(157, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 37);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // JakasNazwa
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.checkBox1);
             this.Name = "JakasNazwa";
@@ -69,5 +80,6 @@
 
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
